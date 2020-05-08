a58948810fa7350f14a2c705f6434608051d1fd2
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-08 13:39:26 -0700

    Finished the form with on change handler and on submit handler, sending a prop up to app so that it will add the new form to that state.

--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TeamForm = () => {
+const TeamForm = (props) => {
+    const [formData, setFormData] = useState({
+        id: Date.now(),
+        name: '',
+        email: '',
+        role: ''
+    })
+
+    const changeHandler = event => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+        props.onSubmit(formData);
+        setFormData({
+            id: Date.now(),
+            name: '',
+            email: '',
+            role: ''
+        })
+    }
+
+    console.log(formData);
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <label htmlFor="name">Name: </label>
-            <input id="name" name="name" type="text" />
+            <input 
+            id="name" 
+            name="name" 
+            type="text" 
+            value={formData.name}
+            onChange={changeHandler}
+            />
 
             <label htmlFor="email"> Email: </label>
-            <input id="email" name="email" type="email" />
+            <input 
+            id="email" 
+            name="email" 
+            type="email"
+            value={formData.email}
+            onChange={changeHandler}
+            />
 
            <label>
                 Role: 
-                <select>
-                    <option value=""></option>
+                <select value={formData.role} name='role' onChange={changeHandler}>
+                    <option value=''></option>
                     <option value="React Developer">React Developer</option>
                     <option value="Data scientist">Data Scientist</option>
                     <option value="IOS Developer">IOS Developer</option>
@@ -20,6 +58,7 @@ const TeamForm = () => {
                     <option value="Gopher">Gopher</option>
                 </select>
            </label>
+           <button type='submit'>Submit Form</button>
         </form>
     )
 }

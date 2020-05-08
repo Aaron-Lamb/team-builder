9d187dc61aabb10a440765af858d87989c696450
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-05-08 16:17:35 -0700

    Added an edit button with an onclick that takes props to the Members component

--- team-builder/src/components/Member.js
+++ team-builder/src/components/Member.js
@@ -8,6 +8,7 @@ const Members = (props) => {
                     <h3>{member.name}</h3>
                     <h4>{member.email}</h4>
                     <h4>{member.role}</h4>
+                    <button onClick={() => props.memberToEdit}>Edit team member</button>
                 </div>
              ))}
         </div>
